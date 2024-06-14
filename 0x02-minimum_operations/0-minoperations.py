580c9e74e104d4ef7d2e8d8bd9e8231a603a6e6f
Jeanluchabi <jeanuchabi@gmail.com>
2024-06-14 02:46:43 +0200

    an update

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,28 +1,30 @@
 #!/usr/bin/python3
-""" This  calculates the fewest number of operations needed to result
-in exactly n H characters in the file"""
+'''The minimum operations coding challenge.
+'''
 
 
 def minOperations(n):
-    """This computes the fewest number of operations needed to result in exactly
-    n H characters
-    """
-    if n <= 1:
+    '''Computes the fewest number of operations needed to result
+    in exactly n H characters.
+    '''
+    if not isinstance(n, int):
         return 0
-    
-    # Initialize an array to store the minimum number of operations
-    dp = [0] * (n + 1)
-    
-    # Iterate from 2 to n
-    for i in range(2, n + 1):
-        dp[i] = float('inf')  # Initialize with infinity or a large number
-        
-        # Find the best way to achieve i 'H' characters
-        for j in range(1, i):
-            if i % j == 0:
-                dp[i] = min(dp[i], dp[j] + i // j)
-    
-    return dp[n]
-
-
+    ops_counting = 0
+    clipb = 0
+    done = 1
+    # print('H', end='')
+    while done < n:
+        if clipb == 0:
+            clipb = done
+            done += clipb
+            ops_count += 2
+        elif n - done > 0 and (n - done) % done == 0:
+            clipb = done
+            done += clipb
+            ops_count += 2
+        elif clipb > 0:
+            # paste
+            done += clipb
+            ops_counting += 1
+    return ops_count
 
